250610aebfb55cbcf087ed2dc83e8ed2322c143c
Gregory Petrosyan <gregory.petrosyan@gmail.com>
2019-02-14 13:17:07 +0300

    More thorough shrink tests

--- shrink_test.go
+++ shrink_test.go
@@ -12,6 +12,8 @@ import (
 	"testing"
 )
 
+const shrinkTestRuns = 10
+
 func TestShrink_Trivial(t *testing.T) {
 	checkShrink(t, Bind(func(t *T, i int) {
 		if i > 1000000 {
@@ -102,15 +104,19 @@ func TestMinimize_LowerBound(t *testing.T) {
 func checkShrink(t *testing.T, prop func(*T), draws ...Value) {
 	t.Helper()
 
-	_, _, buf, err1, err2 := doCheck(t, prop)
-	if err1 == nil && err2 == nil {
-		t.Fatalf("shrink test did not fail")
-	}
-	if traceback(err1) != traceback(err2) {
-		t.Fatalf("flaky shrink test\nTraceback (%v):\n%v\nOriginal traceback (%v):\n%v", err2, traceback(err2), err1, traceback(err1))
-	}
+	for i := 0; i < shrinkTestRuns; i++ {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			_, _, buf, err1, err2 := doCheck(t, prop)
+			if err1 == nil && err2 == nil {
+				t.Fatalf("shrink test did not fail")
+			}
+			if traceback(err1) != traceback(err2) {
+				t.Fatalf("flaky shrink test\nTraceback (%v):\n%v\nOriginal traceback (%v):\n%v", err2, traceback(err2), err1, traceback(err1))
+			}
 
-	_ = checkOnce(newT(t, newBufBitStream(buf, false), false, draws...), prop)
+			_ = checkOnce(newT(t, newBufBitStream(buf, false), false, draws...), prop)
+		})
+	}
 }
 
 func pack(fields ...Value) Value {
