b108d0f4eb51624956e11f1cb6d94d3248615ab4
Gregory Petrosyan <gregory.petrosyan@gmail.com>
2019-02-20 11:10:00 +0300

    Better float example 'tests'

--- floats_external_test.go
+++ floats_external_test.go
@@ -5,25 +5,41 @@
 package rapid_test
 
 import (
+	"math"
+	"sort"
 	"testing"
 
 	. "github.com/flyingmutant/rapid"
 )
 
+const floatExampleFormat = "%30g %10.3g % 5d % 20d % 16x"
+
 func TestFloat32sExamples(t *testing.T) {
 	g := Float32s()
 
+	var vals []float64
 	for i := 0; i < 100; i++ {
-		f, _, _ := g.Example()
-		t.Log(f)
+		f_, _, _ := g.Example()
+		vals = append(vals, float64(f_.(float32)))
+	}
+	sort.Float64s(vals)
+
+	for _, f := range vals {
+		t.Logf(floatExampleFormat, f, f, int(math.Log10(math.Abs(f))), int64(f), math.Float32bits(float32(f)))
 	}
 }
 
 func TestFloat64sExamples(t *testing.T) {
 	g := Float64s()
 
+	var vals []float64
 	for i := 0; i < 100; i++ {
-		f, _, _ := g.Example()
-		t.Log(f)
+		f_, _, _ := g.Example()
+		vals = append(vals, f_.(float64))
+	}
+	sort.Float64s(vals)
+
+	for _, f := range vals {
+		t.Logf(floatExampleFormat, f, f, int(math.Log10(math.Abs(f))), int64(f), math.Float64bits(f))
 	}
 }
